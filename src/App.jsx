540499179647a72407f307890e9335d9eb121a5a
Ethan Ho <hoea2@uci.edu>
2024-02-07 20:39:47 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route></Route>
+                </Routes>
+            </Router>
+        </>
+    )
+}
+
+
+export default App;
